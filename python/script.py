c4820e9507b7dd87b36c196495581a498d3adbea
Zach <zhipps@gmail.com>
2014-12-06 03:06:57 +0000

    added christmas lights page and created css stylesheet

--- python/script.py
+++ python/script.py
@@ -1,13 +1,18 @@
 import webiopi
 import datetime
-#from nrf24 import NRF24
+from nrf24 import NRF24
 import time
 
 GPIO = webiopi.GPIO
+relay = 11
 
+HOUR_ON = 17
+HOUR_OFF = 23
 
 # setup function is automatically called at WebIOPi startup
 def setup():
+
+    GPIO.setFunction(relay, GPIO.OUT)
    # pipes = [[0xe7, 0xe7, 0xe7, 0xe7, 0xe7], [0xc2, 0xc2, 0xc2, 0xc2, 0xc2]]
    # radio = NRF24()
    # radio.begin(1, 0,"P8_23","P8_24")
@@ -23,15 +28,27 @@ def setup():
     return True
 # loop function is repeatedly called by WebIOPi 
 def loop():
-	
+    	
+    # retrieve current datetime
+    now = datetime.datetime.now()
 
-	
-	
+    # toggle light ON all days at the correct time
+    if ((now.hour == HOUR_ON) and (now.minute == 0)):
+        if (GPIO.digitalRead(relay) == GPIO.HIGH):
+            GPIO.digitalWrite(relay, GPIO.LOW)
 
-		
-    return True
+    # toggle light OFF
+    if ((now.hour == HOUR_OFF) and (now.minute == 0)):
+        if (GPIO.digitalRead(relay) == GPIO.LOW):
+            GPIO.digitalWrite(relay, GPIO.HIGH)
+
+    # gives CPU some time before looping again
+    webiopi.sleep(1)
+
 # destroy function is called at WebIOPi shutdown
-# def destroy():
+def destroy():
+    GPIO.digitalWrite(LIGHT, GPIO.HIGH)
+    return True
 
 #def dataRX(RXbuf):
 #	radio.startListening()
